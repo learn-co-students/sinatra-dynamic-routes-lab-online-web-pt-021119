22c3782b49c9edf6797a1a133e3ef8ad9024fa4d
cyantis <cyantis@gmail.com>
2019-05-23 18:34:41 -0600

    Done.

--- app.rb
+++ app.rb
@@ -14,10 +14,13 @@ class App < Sinatra::Base
 
   get '/say/:number/:phrase' do
     @number = params[:number].to_i
-    @phrase = params[:phrase]
+    @phrase_arr = []
     @number.times do
-      <p>"#{@phrase}"</p>
+      @phrase_arr << params[:phrase]
     end
+    @phrase = @phrase_arr.join(" ")
+
+    "#{@phrase}"
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
